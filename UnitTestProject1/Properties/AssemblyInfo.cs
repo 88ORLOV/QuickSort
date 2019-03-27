7a2d2b060b3e05a5a692708239a916cd9cad7db2
88ORLOV <44868289+88ORLOV@users.noreply.github.com>
2019-03-27 21:53:59 +0500

    Add files via upload

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bdb55fd8-a987-492c-a798-2ebaafb6ef86")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/QiuckSort.Test.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        [TestMethod]
+        public void QuickSortTest3()
+        {
+            var array = new int[] { 3, 2, 1 };
+            QuickSort.Program.QuickSort(array);
+
+            Assert.IsTrue(array[1] > array[0]);
+            Assert.IsTrue(array[2] > array[1]);
+        }
+
+        [TestMethod]
+        public void QuickSortTest100()
+        {
+
+            var array = QuickSort.Program.GenerateArray(100);
+            bool b = false;
+
+            QuickSort.Program.QuickSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] <= array[i])
+                {
+                    b = true;
+                }
+                Assert.AreEqual(b, true);
+                b = false;
+            }
+
+        }
+
+        [TestMethod]
+        public void QuickSortTest1000()
+        {
+            var array = new int[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                array[i] = 555;
+            }
+
+            QuickSort.Program.QuickSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(array[i], 555);
+            }
+        }
+
+        [TestMethod]
+        public void QuickSortTestNull()
+        {
+            var array = new int[0];
+            bool b = false;
+
+            QuickSort.Program.QuickSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] <= array[i])
+                {
+                    b = true;
+                }
+                Assert.AreEqual(b, true);
+                b = false;
+            }
+
+        }
+    }
+}
+
